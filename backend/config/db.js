63bdfdbf0ddf2e7182dbfbd461556a10d82efccf
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-03 17:15:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://sabuqatada:Nahipata_01@ecommerce.glnuxdl.mongodb.net/?retryWrites=true&w=majority&appName=ECommerce");
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
