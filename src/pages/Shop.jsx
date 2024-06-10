f46ead3ca9388665e3c3efa4e189b08e7eeb8ceb
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-11 01:39:21 +0530

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,20 +1,8 @@
-import { AiFillAccountBook, AiOutlineFilter } from "react-icons/ai";
-
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useGetFilteredProductsQuery } from "../redux/api/productApiSlice";
 import { useFetchCategoriesQuery } from "../redux/api/categoryApiSlice";
 
-import React from "react";
-
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-import ListItem from "@mui/material/ListItem";
-import { AiFillFilter } from "react-icons/ai";
-
-import { Link } from "react-router-dom";
-import List from "@mui/material/List";
 import {
   setCategories,
   setProducts,
@@ -87,29 +75,17 @@ const Shop = () => {
     ),
   ];
 
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
-
   const handlePriceChange = (e) => {
     // Update the price filter state when the user types in the input filed
     setPriceFilter(e.target.value);
   };
 
-
-  const DrawerList = (
-    <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        <ListItem>
-        /* <div className="container mx-auto mt-9">
+  return (
+    <>
+      <div className="container mx-auto">
         <div className="flex md:flex-row">
-          <div className="bg-[#f1f0f0]   rounded-lg p-1 mt-2 mb-2">
-            <h1 className="md:hidden top-3 lg:hidden sm:absolute h-[2rem] sm:left-[17rem] sm:bg-[#f1f0f0] rounded-lg p-1  w-[4rem] text-canter">
-             &nbsp;Filters 
-            </h1>
-            <h2 className="h4 text-center py-2 text-white bg-pink rounded-full mb-2">
+          <div className="bg-[#dcdbdb] p-3 mt-2 mb-2">
+            <h2 className="h4 text-center py-2 bg-white rounded-full mb-2">
               Filter by Categories
             </h2>
 
@@ -121,12 +97,12 @@ const Shop = () => {
                       type="checkbox"
                       id="red-checkbox"
                       onChange={(e) => handleCheck(e.target.checked, c._id)}
-                      className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
 
                     <label
                       htmlFor="pink-checkbox"
-                      className="ml-2 text-sm font-medium capitalize text-black dark:text-gray-300"
+                      className="ml-2 text-sm font-medium text-black dark:text-gray-300"
                     >
                       {c.name}
                     </label>
@@ -135,7 +111,7 @@ const Shop = () => {
               ))}
             </div>
 
-            <h2 className="h4 text-center py-2 text-white bg-pink rounded-full mb-2">
+            <h2 className="h4 text-center py-2 bg-white rounded-full mb-2">
               Filter by Brands
             </h2>
 
@@ -153,7 +129,7 @@ const Shop = () => {
 
                     <label
                       htmlFor="pink-radio"
-                      className="ml-2 text-sm font-medium text-black bg-pinke "
+                      className="ml-2 text-sm font-medium text-black dark:text-gray-300"
                     >
                       {brand}
                     </label>
@@ -162,7 +138,7 @@ const Shop = () => {
               ))}
             </div>
 
-            <h2 className="h4 text-center py-2 text-white bg-pink rounded-full mb-2">
+            <h2 className="h4 text-center py-2 bg-white rounded-full mb-2">
               Filer by Price
             </h2>
 
@@ -172,7 +148,7 @@ const Shop = () => {
                 placeholder="Enter Price"
                 value={priceFilter}
                 onChange={handlePriceChange}
-                className="w-full text-black px-3 py-2 placeholder-gray-400 border rounded-lg focus:outline-none focus:ring focus:border-pink-300"
+                className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg focus:outline-none focus:ring focus:border-pink-300"
               />
             </div>
 
@@ -201,38 +177,7 @@ const Shop = () => {
             </div>
           </div>
         </div>
-      </div> */
-
-        </ListItem>
-      </List>
-    </Box>
-  );
-
-  return (
-    <>
-    <div>
-      <Button onClick={toggleDrawer(true)}>
-      <AiFillFilter className="text-4xl text-green-600 border-[0.25px] rounded-lg border-[#dad9d9e3] p-1">
-      <p>Filter</p>  </AiFillFilter>
-      </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
-    <div className="p-3 overflow-hidden  flex flex-wrap justify-center">
-            <h2 className="h4 text-center mb-2">{products?.length} Products</h2>
-            <div className="xsm:grid  gap-x-2 gap-y-1 xsm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-              {products.length === 0 ? (
-                <Loader />
-              ) : (
-                products?.map((p) => (
-                  <div className="p-3" key={p._id}>
-                    <ProductCard p={p} />
-                  </div>
-                ))
-              )}
-            </div>
-          </div>
+      </div>
     </>
   );
 };
