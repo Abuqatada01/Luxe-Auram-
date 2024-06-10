141de4c66235f39a4c7e639ca314cdb5bb4c9087
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-10 23:28:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@mui/material/Box','tailwind-merge'],
-    }
-  },
-
   server: {
     proxy: {
       "/api/": "https://watchbackend-9h6h.onrender.com",
       "/uploads/": "https://watchbackend-9h6h.onrender.com",
-      // "/api/": "http://localhost:5000",
-      // "/uploads/": "http://localhost:5000",
     },
   },
-})
-
+});
