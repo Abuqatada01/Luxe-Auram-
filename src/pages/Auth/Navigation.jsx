8d5754c6d8f59d3cbfb394209a7709b54cce8e1b
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-11 01:38:44 +0530

    Update Navigation.jsx

--- src/pages/Auth/Navigation.jsx
+++ src/pages/Auth/Navigation.jsx
@@ -1,46 +1,21 @@
 import { useState } from "react";
-import React from "react";
-
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-// import { Menu } from "@mui/material/Menu";
-
 import {
-  AiFillHome,
-  AiFillShopping,
+  AiOutlineHome,
+  AiOutlineShopping,
   AiOutlineLogin,
   AiOutlineUserAdd,
-  AiOutlineLine,
-  AiFillCarryOut,
-  AiFillCreditCard,
-  // AiFillShopping,
+  AiOutlineShoppingCart,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import "../Auth/Navigation.css";
+import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
 import FavoritesCount from "../Products/FavoritesCount";
 
-
-export default function Sidebar() {
-  const [open, setOpen] = React.useState(false);
-
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
-
+const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const { cartItems } = useSelector((state) => state.cart);
 
@@ -66,45 +41,41 @@ export default function Sidebar() {
     }
   };
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-          <ListItem >
-          
-              <div className="flex flex-col  justify-center">
+  return (
+    <div
+      style={{ zIndex: 9999 }}
+      className={`${
+        showSidebar ? "hidden" : "flex"
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-black bg-[#bdbaba] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      id="navigation-container"
+    >
+      <div className="flex flex-col justify-center space-y-4">
         <Link
           to="/"
-          className="flex text-black font-semibold   items-center transition-transform transform hover:translate-x-2"
+          className="flex items-center transition-transform transform hover:translate-x-2"
         >
-          <AiFillHome
-            color=""
-            className="mr-2 mt-5 border-black  text-xl"
-            size={26}
-          />
-        <span className="mt-6">HOME</span>
+          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
         </Link>
 
         <Link
           to="/shop"
-          className="flex text-black font-semibold items-center transition-transform transform hover:translate-x-2"
+          className="flex items-center transition-transform transform hover:translate-x-2"
         >
-          <AiFillShopping className="mr-2 mt-[3rem]" size={26} />
-          <span className=" mt-[3rem]">SHOP</span>
+          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
         </Link>
 
-        <Link
-          to="/cart"
-          className="flex relative  text-black font-semibold items-center transition-transform transform hover:translate-x-2 "
-        >
+        <Link to="/cart" className="flex relative">
           <div className="flex items-center transition-transform transform hover:translate-x-2">
-            <AiFillCarryOut className="mt-[3rem] mr-2" size={26} />
-            <span className=" mt-[3rem]">Cart</span>{" "}
+            <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
+            <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
           </div>
 
           <div className="absolute top-9">
             {cartItems.length > 0 && (
               <span>
-                <span className="px-1 py-0 text-sm text-white bg-purple-500 rounded-full">
+                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
                   {cartItems.reduce((a, c) => a + c.qty, 0)}
                 </span>
               </span>
@@ -113,33 +84,137 @@ export default function Sidebar() {
         </Link>
 
         <Link to="/favorite" className="flex relative">
-          <div className="flex text-black font-semibold items-center transition-transform transform hover:translate-x-2">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
             <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className=" mt-[3rem]">
+            <span className="hidden nav-item-name mt-[3rem]">
               Favorites
             </span>{" "}
             <FavoritesCount />
           </div>
         </Link>
       </div>
-         
-          </ListItem>
-      
-      </List>
-    
-    </Box>
-  );
 
-  return (
-    <div>
-      <Button onClick={toggleDrawer(true)}>
-      <img className=" absolute h-8 w-8 -top-[3.5rem]" src="https://tse4.mm.bing.net/th?id=OIP.6E_njRMJUwSjSkWsmqeWPAAAAA&pid=Api&P=0&h=180">
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-800 focus:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+          {userInfo && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-4 w-4 ml-1 ${
+                dropdownOpen ? "transform rotate-180" : ""
+              }`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+              />
+            </svg>
+          )}
+        </button>
 
-      </img>
-        </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
+        {dropdownOpen && userInfo && (
+          <ul
+            className={`absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 ${
+              !userInfo.isAdmin ? "-top-20" : "-top-80"
+            } `}
+          >
+            {userInfo.isAdmin && (
+              <>
+                <li>
+                  <Link
+                    to="/admin/dashboard"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/admin/productlist"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/admin/categorylist"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Category
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/admin/orderlist"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Orders
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/admin/userlist"
+                    className="block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Users
+                  </Link>
+                </li>
+              </>
+            )}
+
+            <li>
+              <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
+                Profile
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={logoutHandler}
+                className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        )}
+        {!userInfo && (
+          <ul>
+            <li>
+              <Link
+                to="/login"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
+                <span className="hidden nav-item-name">LOGIN</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/register"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+              >
+                <AiOutlineUserAdd size={26} />
+                <span className="hidden nav-item-name">REGISTER</span>
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default Navigation;
