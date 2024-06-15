dbe085eb6aca97dee76ed473a938afb19fa75d12
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-16 01:25:21 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+/*
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../redux/actions/productActions';
@@ -20,6 +20,42 @@ const Home = () => {
       {loading ? (
         <h2>Loading...</h2>
       ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <div>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;*/
+
+// src/pages/Home.jsx
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../redux/api/productApiSlice';
+import Product from '../components/Product';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { products, status, error } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchProducts());
+    }
+  }, [status, dispatch]);
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      {status === 'loading' ? (
+        <h2>Loading...</h2>
+      ) : status === 'failed' ? (
         <h2>{error}</h2>
       ) : (
         <div>
