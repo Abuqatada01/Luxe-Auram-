31064d6a4b7fc7c782cc4d00b5b892c88f5416c3
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-16 01:29:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { Outlet } from "react-router-dom";
+/*import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "../src/pages/Modern/Navbar"
 import Sidebar from "./pages/Modern/Sidebard";
 import Test from "./pages/Auth/test";
+*/
+import Home from "./pages/Home"
 
 const App = () => {
   return (
     <>
-      <ToastContainer />
-      <Navbar/>
-      <Navigation />
-      <main className="py-3 overlow-hidden">
-        <Outlet />
-      </main>
- {/* <Test/> */}
+      <Home/>
+ 
     </>
   );
 };
