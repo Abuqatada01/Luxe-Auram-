2bd479fa69c0a754b7f5c7dd6d0973e1b9811c89
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-16 00:53:38 +0530

    Create api.js

---
+++ src/Utils/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'https://watchbackend-9h6h.onrender.com';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+export default api;
