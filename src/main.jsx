2c954068fc581772748616ca346cb82096890b5b
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-16 01:26:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+/*import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
@@ -49,7 +49,7 @@ const router = createBrowserRouter(
       <Route path="/shop" element={<Shop />} />
 
       {/* Registered users */}
-      <Route path="" element={<PrivateRoute />}>
+ /*     <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
         <Route path="/shipping" element={<Shipping />} />
         <Route path="/placeorder" element={<PlaceOrder />} />
@@ -73,6 +73,88 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
+  </Provider>
+);
+*/
+
+
+// src/index.js
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+// Auth
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+// Admin
+import AdminRoute from './pages/Admin/AdminRoute';
+import Profile from './pages/User/Profile';
+import UserList from './pages/Admin/UserList';
+import CategoryList from './pages/Admin/CategoryList';
+import ProductList from './pages/Admin/ProductList';
+import AllProducts from './pages/Admin/AllProducts';
+import ProductUpdate from './pages/Admin/ProductUpdate';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import OrderList from './pages/Admin/OrderList';
+
+// User
+import Home from './pages/Home';
+import Favorites from './pages/Products/Favorites';
+import ProductDetails from './pages/Products/ProductDetails';
+import Cart from './pages/Cart';
+import Shop from './pages/Shop';
+
+// Orders
+import Shipping from './pages/Orders/Shipping';
+import PlaceOrder from './pages/Orders/PlaceOrder';
+import Order from './pages/Orders/Order';
+import UserOrder from './pages/User/UserOrder';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/favorite" element={<Favorites />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/shop" element={<Shop />} />
+
+      {/* Registered users */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order/:id" element={<Order />} />
+        <Route path="/user-orders" element={<UserOrder />} />
+      </Route>
+
+      {/* Admin routes */}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="productlist/:pageNumber" element={<ProductList />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
+        <Route path="orderlist" element={<OrderList />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PayPalScriptProvider>
       <RouterProvider router={router} />
