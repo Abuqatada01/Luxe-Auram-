8d67fb051e54eb5a2a5338a6b95d27ff6eb0abc9
Abuqatada01 <126242403+Abuqatada01@users.noreply.github.com>
2024-06-16 00:59:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,74 +1,35 @@
-import { Link, useParams } from "react-router-dom";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import Header from "../components/Header";
-import Product from "./Products/Product";
 
-// Hardcoded list of products
-const staticProducts = [
-  { _id: '1', name: 'Product 1', price: 29.99 },
-  { _id: '2', name: 'Product 2', price: 49.99 },
-  { _id: '3', name: 'Product 3', price: 19.99 },
-  // Add more products as needed
-];
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../redux/actions/productActions';
+import Product from '../components/Product';
 
 const Home = () => {
-  // Use useParams to get the keyword from the URL
-  const { keyword } = useParams();
+  const dispatch = useDispatch();
 
-  // Since this is static, we're not loading or fetching data
-  const isLoading = false;
-  const isError = false;
-  const error = null;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-  // Use static products list instead of fetched data
-  const products = staticProducts;
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
-    <>
-      {/* Conditionally render the Header component if there's no keyword */}
-      {!keyword && <Header />}
-      
-      {/* Handle loading state */}
-      {isLoading ? (
-        <Loader />
-      ) : isError ? (
-        // Handle error state
-        <Message variant="danger">
-          {error?.data?.message || error?.message || "An error occurred"}
-        </Message>
+    <div>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
       ) : (
-        // Handle successful data fetching (static in this case)
-        <>
-          <div className="flex justify-between items-center">
-            <h1 className="lg:ml-[20rem] xsm:ml-[2rem] xsm:mr-[4rem] lg:mt-[10rem] lg:text-[4rem] xsm:text-[2rem]">
-              Special Products
-            </h1>
-            <Link
-              to="/shop"
-              className="bg-green-600 text-white font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
-            >
-              Shop
-            </Link>
-          </div>
-          
-          <div className="flex justify-center flex-wrap xsm:mr-[4rem] mt-[2rem]">
-            {/* Render static products */}
-            {products.length > 0 ? (
-              products.map((product) => (
-                <div key={product._id}>
-                  <Product product={product} />
-                </div>
-              ))
-            ) : (
-              <Message variant="info">No products found</Message>
-            )}
-          </div>
-        </>
+        <div>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
 export default Home;
-
